45c81e293e4633dc534bfb7b1b8a9f3cab6e224d
Sergey Resh <resh.s.v@yandex.ru>
2024-03-03 15:45:41 +0400

    refactoring direction JS

--- dist/js/direction.js
+++ dist/js/direction.js
@@ -5,14 +5,22 @@ window.addEventListener('DOMContentLoaded', () => {
     const city = document.querySelectorAll('.direction__select__city'),
           ports = document.querySelectorAll('.direction__port__wrapper'),
           maps = document.querySelectorAll('.direction__map__city');
-    
-        city.forEach((item, i) => {
-            item.addEventListener('click', () => {
+
+    let currentCity = 0;
+    city[currentCity].style.borderBottom = `4px solid #ffd289`;
+          
+    city.forEach((item, i) => {
+        item.addEventListener('click', () => {
                 ports.forEach((port) => port.classList.add('invisible'));
                 ports[i].classList.remove('invisible');
                 maps.forEach((map) => map.classList.add('invisible'));
-                maps[i].classList.remove('invisible');              
-            });
+                maps[i].classList.remove('invisible');
+
+                city.forEach(item => {
+                    item.style.borderBottom = `4px solid #3e5e82`;
+                })
+                city[i].style.borderBottom = `4px solid #ffd289`;
         });
+    });
 
 });
